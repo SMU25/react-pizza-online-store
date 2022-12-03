a51517b5a756ce01fbd9995a95ad5a0dc6aba6eb
Oleksandr Myronchuk <smironcuk35@gmail.com>
2022-12-03 15:51:03 +0200

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./public/db.json");
 const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
